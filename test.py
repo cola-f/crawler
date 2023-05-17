dcc74e665dffd861feb96701dd92c51dd86a1f32
colaf <colaf@colaf.net>
2023-05-17 18:00:07 +0900

    git test

--- test.py
+++ test.py
@@ -9,6 +9,3 @@ b = tf.variable(0.2)
 
 opt = tf.keras.optimizers.Adam(learning_rate = 0.1))
 #경사하강법을 사용해서 변수를 업데이트하는 도구
-def lossF():
-    return tf.square(실제값 - 예측값)
-opt.minimize(lossF, var_list[a, b]))
