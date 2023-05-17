becc9047c38990a0dc65fbded22e26c93821ac3d
colaf <colaf@colaf.net>
2023-05-17 17:42:46 +0900

    modify

--- lecture03_neural_network.py
+++ lecture03_neural_network.py
@@ -1,3 +1,16 @@
 import tensorflow as tf
+import pandas as pd
 
-tf.keras.models.Sequencial([])
+data = pd.read_csv('gpascore.csv')
+print(data)
+exit()
+
+model = tf.keras.models.Sequencial([
+    tf.keras.layers.Dense(64, activation='sigmoid'),
+    tf.keras.layers.Dense(128, activation='sigmoid'),
+    tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+#model.fit(입력 데이터, 출력 데이터, epochs=10)
