b431f83535db50cead48d14699a8c47d1dd4226d
colaf <colaf@colaf.net>
2023-05-20 00:16:17 +0900

    2023-05-20 00:16:99

--- lecture03_neural_network.py
+++ lecture03_neural_network.py
@@ -2,8 +2,13 @@ import tensorflow as tf
 import pandas as pd
 
 data = pd.read_csv('gpascore.csv')
-print(data)
-exit()
+print(data.isnull().sum())
+data = data.dropna()    #빈칸 데이터를 없애준다.
+
+y데이터 = data['admit'].values
+x데이터 = []
+for i, rows in data.iterrows():
+    x데이터.append([rows['gre'], rows['gpa'], rows['rank']])
 
 model = tf.keras.models.Sequencial([
     tf.keras.layers.Dense(64, activation='sigmoid'),
