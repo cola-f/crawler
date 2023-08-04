1d7c05870f88707cf6e26a7738b47aa0af81087f
colaf <colaf@colaf.net>
2023-08-05 01:58:03 +0900

    lecture03

--- lecture03_neural_network.py
+++ lecture03_neural_network.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import pandas as pd
 
@@ -10,7 +11,7 @@ x데이터 = []
 for i, rows in data.iterrows():
     x데이터.append([rows['gre'], rows['gpa'], rows['rank']])
 
-model = tf.keras.models.Sequencial([
+model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='sigmoid'),
     tf.keras.layers.Dense(128, activation='sigmoid'),
     tf.keras.layers.Dense(1, activation='sigmoid'),
@@ -18,4 +19,8 @@ model = tf.keras.models.Sequencial([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-#model.fit(입력 데이터, 출력 데이터, epochs=10)
+model.fit(np.array(x데이터), np.array(y데이터), epochs=1000)
+
+예측값 = model.predict(np.array([[750, 3.70, 3], [400, 2.2, 1]]))
+
+print(예측값)
